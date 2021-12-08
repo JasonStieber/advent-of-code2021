ae9b9c6dcccd0874ff940e65f7bb870850bbabef
Jason StStieber <jrstiebeber@gmail.com>
2021-12-08 14:06:02 -0800

    preprocessed the data and built initital file structure

---
+++ day5/go.mod
@@ -0,0 +1,3 @@
+module 2021AdventOfCode/day5
+
+go 1.17
---
+++ day5/ventlines.go
@@ -0,0 +1,7 @@
+package main
+
+import "log"
+
+func main() {
+	log.Printf("This is the first cordinate (%v, %v)", lines[0].start.x, lines[0].start.y)
+}
